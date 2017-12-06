2eb387eb8880944152fbfad86abc796f24e42e05
Eric Hegeman <miracleofmagick@gmail.com>
2017-12-06 00:51:18 -0500

    added form and encrypt function to main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from caesar import rotate_string
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = '''<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+      <form method="post">
+          <label for"rot">Rotate by:</label>
+          <input type="text" name="rot" id="rot" value="0">
+          <textarea name="text" id="text"></textarea>
+          <input type="submit" value="Submit">
+      </form>
+    </body>
+</html>'''
+@app.route("/")
+def index():
+    return form
+
+@app.route("/", methods=['post'])
+def encrypt():
+    text=request.form['text']
+    rot=int(request.form['rot'])
+    return "<h1>" + rotate_string(text, rot) + "</h1>"
+
+app.run()
